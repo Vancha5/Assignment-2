8e745d1bca04b51208c3bc19f14593b43b6b4d13
Vancha5 <vancha56@gmail.com>
2015-10-09 03:13:58 -0700

    sixth commit

--- Calculator/app/src/main/java/com/example/t00192790/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/t00192790/calculator/MainActivity.java
@@ -10,14 +10,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOError;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvmain;
    //button num_1;
-    double value = 0;
-    double result = 0;
+    double value;
+    double result;
     String op;
+    String str;
+    double tempvalue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,30 +30,74 @@ public class MainActivity extends AppCompatActivity {
        // num_1 = (Button)findViewById(R.id.button19);
         //num_1.setOnClickListener(num_handler);
         tvmain = (TextView) findViewById(R.id.textViewmain);
+        tvmain.setText("");
+        value = 0;
+        result = 0;
+        op = "";
+        str = tvmain.getText().toString();
+        tempvalue = 0;
+
 
     }
 
         public void onClick(View v) {
-            Toast.makeText(getBaseContext(), "Number Handler", Toast.LENGTH_SHORT).show();
             Button b1 = (Button) v;
-            tvmain.append(b1.getText());
+
+            if (str.contains(".")) { //checks if the string contains a decimal, if it does 0's are ok
+               // if (Double.parseDouble(tvmain.getText().toString()) == 0)
+                    tvmain.append(b1.getText());
+            }
+            else if(str != null && !str.isEmpty()) {
+                if (Double.parseDouble(str) == 0) //checks if the value is equal to zero, if it is I need an if to not allow zeros
+                {
+                    if (b1.getText().toString().equals("0"))
+                        return;
+                    else
+                        tvmain.append(b1.getText());
+                }
+                else
+                    tvmain.append(b1.getText());
+            }
+            else //if no decimal and value is not zero then just append, can also be empty
+                tvmain.append(b1.getText());
         }
+
+
 
         public void opOnClick(View v) {
             Button b2 = (Button) v;
-            op = b2.getText().toString();
-            if(value == 0) {
-                value = Double.parseDouble(tvmain.getText().toString());
-                tvmain.setText("");
+            str = tvmain.getText().toString();
+            tempvalue = 0;
+            if (!str.isEmpty() && !str.equals(null)) {
+
+                op = b2.getText().toString();
+
+                if (value == 0) { //if the string empty then get the value and set the text to empty string
+                    value = Double.parseDouble(str);
+                    tvmain.setText("");
+                    //Toast.makeText(getApplicationContext(), "opOnClick op is not previously set", Toast.LENGTH_SHORT).show();
+                } else if (value == Double.parseDouble(str)) { //if the value is not zero then call the calc method, calc sets the text to the result of calc
+                    tvmain.setText("");
+                    tempvalue = Double.parseDouble(str);
+                } else {
+                    value = calc();
+                    if (tempvalue != 0) {
+                        tvmain.setText(String.valueOf(value));
+                        //  tvmain.setHint(String.valueOf(value));
+                    } else {
+                        tvmain.setText("");
+                        Toast.makeText(getApplicationContext(), "opOnClick value not empty op set to empty string", Toast.LENGTH_SHORT).show();
+
+                    }
+                tempvalue = 0;
+
+                }
             }
-            else {
-                value = calc();
-
-            }
-
-
-
         }
+
+
+
+
 
         public void clear(View v){
             tvmain.setText("");
@@ -60,47 +109,71 @@ public class MainActivity extends AppCompatActivity {
         }
         public void del(View v)
         {
-            String str = tvmain.getText().toString();
-            if (str.length() > 0) {
-                str = str.substring(0, str.length()-1);
+             String tempString = tvmain.getText().toString();
+
+            if(tempString.equals(".") || tempString.equals("-")) {
+                tempString = "";
+
             }
-            tvmain.setText(str);
+
+            else if (tempString.length() > 0) {
+
+                tempString = tempString.substring(0, tempString.length()-1);
+               // Toast.makeText(getApplicationContext(), "str else if", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "ignored if statements", Toast.LENGTH_SHORT).show();
+
+            }
+            tvmain.setText(tempString);
+
         }
 
         public void sign(View v)
         {
-            if(Double.parseDouble(tvmain.getText().toString()) > 0)
-                tvmain.setText("-" + tvmain.getText());
-
+            String tempString = "";
+            if(!str.isEmpty() && str !=null) {
+                if (Double.parseDouble(str) > 0 && (str.length() < 1))
+                    tvmain.setText("-" + str);
+            }
+            else
+            {
+                tempString = "-";
+                tvmain.setText(tempString);
+            }
         }
-        public void calcButton(){
-            double test = calc();
+        public void calcButton(View v){
+             value = calc();
+            if(value != 0)
+             tvmain.setText(String.valueOf(value));
+            else
+                tvmain.setText("");
         }
 
 
         public double calc(){
 
-            if(op.equals("+"))
-            {
-                result = value +  Double.parseDouble(tvmain.getText().toString());
-            }
-            else if(op.equals("-"))
-            {
-                result = value - Double.parseDouble(tvmain.getText().toString());
-            }
-            else if(op.equals("*"))
-            {
-                result = value * Double.parseDouble(tvmain.getText().toString());
-            }
-            else if(op.equals("/"))
-            {
-                result = value / Double.parseDouble(tvmain.getText().toString());
+            if(!str.isEmpty() && !str.equals(null) && !op.equals("")){
+
+            if (op.equals("+")) {
+                result = value + Double.parseDouble(str);
+            } else if (op.equals("-")) {
+                result = value - Double.parseDouble(str);
+            } else if (op.equals("*")) {
+                result = value * Double.parseDouble(str);
+            } else if (op.equals("/")) {
+                result = value / Double.parseDouble(str);
             }
 
-            tvmain.setText(String.valueOf(result));
+
+          //  tvmain.setText(String.valueOf(result));
+                return result;
+        }
+            else if(!str.isEmpty() && !str.equals(null)) {
+
+            result = Double.parseDouble(str);
             return result;
-
-
+        }
+            else
+                return 0;
         }
 
 
